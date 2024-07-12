a7c192ab25919cfa0ff09a9219a92c07cd4f39d1
AlaaYounes <107075054+AlaaYounes@users.noreply.github.com>
2024-07-12 10:36:25 +0300

    adding connectivity plus package.

--- lib/products/data/data_sources/product_dataSource_impl.dart
+++ lib/products/data/data_sources/product_dataSource_impl.dart
@@ -1,30 +1,30 @@
-
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
 import 'package:route_flutter_task/core/utils/api_constants.dart';
 import 'package:route_flutter_task/products/data/models/ProductResponseDTO.dart';
 import 'package:route_flutter_task/products/domain/repositories/product_dataSource.dart';
-import 'package:http/http.dart' as http;
 
 class ProductDataSourceImpl implements ProductDataSource {
   @override
   Future<Either<BaseError, ProductResponseDto>> getProducts() async {
     final connectivityResult = await (Connectivity().checkConnectivity());
-    print(connectivityResult.toString());
-
+    if (connectivityResult.contains(ConnectivityResult.mobile) ||
+        connectivityResult.contains(ConnectivityResult.wifi)) {
       const url = ApiConstants.baseUrl + ApiConstants.productsApi;
       final finalUrl = Uri.parse(url);
       var responseBody = await http.get(finalUrl);
-      var response =
-      ProductResponseDto.fromJson(jsonDecode(responseBody.body));
+      var response = ProductResponseDto.fromJson(jsonDecode(responseBody.body));
       if (responseBody.statusCode == 200) {
         return Right(response);
       } else {
         return Left(BaseError(errorMessage: 'something went wrong'));
       }
+    } else {
+      return Left(BaseError(errorMessage: 'No Internet Connection'));
+    }
   }
 }
 
--- lib/products/presentation/cubit/cubit.dart
+++ lib/products/presentation/cubit/cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:route_flutter_task/core/utils/dependancy_injection.dart';
-import 'package:route_flutter_task/products/domain/repositories/product_dataSource.dart';
 import 'package:route_flutter_task/products/domain/use_cases/product_useCases.dart';
 import 'package:route_flutter_task/products/presentation/cubit/states.dart';
 
@@ -8,18 +7,19 @@ class ProductCubit extends Cubit<ProductStates> {
   ProductCubit() : super(ProductInitialState());
   static ProductCubit get(context) => BlocProvider.of(context);
   final _useCase = sl<ProductUseCases>();
-  final _dataSource = sl<ProductDataSource>();
   void getProducts() async {
     emit(ProductLoadingState());
-    final response = await _useCase.getProducts();
-    response.fold((l) {
-      emit(GetProductsErrorState(errorMessage: l.errorMessage));
-    }, (r) {
-      if (r.products == null) {
-        emit(GetProductsErrorState(errorMessage: 'No Products Found'));
-      } else {
-        emit(GetProductsSuccessState(response: r));
-      }
-    });
+    {
+      final response = await _useCase.getProducts();
+      response.fold((l) {
+        emit(GetProductsErrorState(errorMessage: l.errorMessage));
+      }, (r) {
+        if (r.products == null) {
+          emit(GetProductsErrorState(errorMessage: 'No Products Found'));
+        } else {
+          emit(GetProductsSuccessState(response: r));
+        }
+      });
+    }
   }
 }
